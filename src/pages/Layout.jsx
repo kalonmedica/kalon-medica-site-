0940018431ab25b237330432b99b7fdba0433cc4
kalonmedica <meriadec.pierre-leseach@outlook.fr>
2025-09-19 16:24:08 +0200

    Add files via upload

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -5,10 +5,11 @@ const Layout = () => {
   return (
     <div className="max-w-5xl mx-auto px-4 py-6">
       <nav className="flex space-x-6 text-blue-700 font-semibold mb-6">
+        <Link to="/">Accueil</Link>
         <Link to="/soins">Soins</Link>
         <Link to="/parcours">Parcours patient</Link>
         <Link to="/honoraires">Honoraires</Link>
-      <Link to="/rdv">Prise de rendez-vous</Link>
+        <Link to="/rdv">Prise de rendez-vous</Link>
         <Link to="/mentions-legales">Mentions légales</Link>
       </nav>
       <Outlet />
